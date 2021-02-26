0659165fe5ffeeb903377e4d5c62489adf18b872
trafoDev <47941216+trafoDev@users.noreply.github.com>
2021-02-26 11:01:47 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,1 +1,3 @@
-ffff
+sudo apt-get -y install docker.io && sudo apt-get -y  install docker-compose && sudo apt-get -y  install golang-go
+sudo systemctl start docker
+sudo usermod -a -G docker $USER
