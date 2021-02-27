3df507b61bcdabd4454b82d6a7194f8c86543019
trafoDev <47941216+trafoDev@users.noreply.github.com>
2021-02-27 14:04:11 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,3 +1,6 @@
 sudo apt-get -y install docker.io && sudo apt-get -y  install docker-compose && sudo apt-get -y  install golang-go
 sudo systemctl start docker
 sudo usermod -a -G docker $USER
+# clone the repo
+git clone https://github.com/trafoDev/easyBC.git
+chmod -R 775 *.sh
