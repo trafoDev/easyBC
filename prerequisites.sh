10b74b27e3349395a74c6ac6ae02e3d76d217fff
trafoDev <47941216+trafoDev@users.noreply.github.com>
2021-02-28 21:57:17 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -4,7 +4,8 @@ sudo kill $(pgrep -f unattended)
 sudo apt-get -y update
 sudo apt-get -y upgrade
 #
-sudo apt-get -y install docker.io && sudo apt-get -y  install docker-compose && sudo apt-get -y  install golang-go
+sudo apt-get -y install docker.io && sudo apt-get -y  install docker-compose 
+#&& sudo apt-get -y  install golang-go
 sudo systemctl start docker
 sudo usermod -a -G docker $USER
 # clone the repo
